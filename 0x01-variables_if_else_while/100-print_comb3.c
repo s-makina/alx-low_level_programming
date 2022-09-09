43e800de8038208b4f33ee55c3e5723ab323b578
Salvation Makina <salvation.developer@gmail.com>
2022-09-09 20:26:32 +0000

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Application entyry point
+ * Description: print number combination
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x < i)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
