ce14c5cfd315a7a2f9d20fe5302bc32d4438d591
Salvation Makina <salvation.developer@gmail.com>
2022-09-09 20:39:46 +0000

    fix

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Application entry point
+ * Description: prints all possible combination
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y, z, g;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 48; y <= 57; y++)
+		{
+			for (z = 48; z <= 57; z++)
+			{
+				for (z = 48; z <= 57; z++)
+				{
+				if (((z + g) > (x + y) &&  z >= x) || x < g)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar(g);
+
+					if (x + y + z + g == 227 && x == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
