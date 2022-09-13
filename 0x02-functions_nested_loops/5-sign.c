326852bf20bb26f717a592ae0162c4d8034090cd
Salvation Makina <salvation.developer@gmail.com>
2022-09-13 18:58:22 +0000

    Prints sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - Prototype
+ * @num: Integer parameter 
+ * Description: prints the sign of the given integer value
+ * Return: Integer
+ */
+
+int print_sign(int num)
+{
+	if (num > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (num < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
