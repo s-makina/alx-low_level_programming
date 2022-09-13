3ce2f5e20329c0678320db58eb123d3e0074c124
Salvation Makina <salvation.developer@gmail.com>
2022-09-13 19:24:37 +0000

    Add two numbers#

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Prototype
+ * @num1: first integer parameter
+ * @num2: second integer parameter
+ * Description: adds two integers and returns the result
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	return num1 + num2;
+}
