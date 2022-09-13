78fd416f9a5f6bf29d47d4f2fd456ea6b97fbbf2
Salvation Makina <salvation.developer@gmail.com>
2022-09-13 19:43:45 +0000

    trainling white space fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,7 @@
 
 /**
  * main - Application Entry point
- * Description - computes and prints the sum of 
- * all the multiples of 3 or 5 below 1024 (excluded),
- * followed by a new line.
+ * Description - computes and prints 
  * Return: 0
  */
 int main(void)
