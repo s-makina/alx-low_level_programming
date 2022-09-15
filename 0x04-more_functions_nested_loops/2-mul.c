a2d81e515aa2d57f497fc083ba059c1b0ca32daa
Salvation Makina <salvation.developer@gmail.com>
2022-09-15 16:12:38 +0000

    mulitiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: first interger value
+ * @b: second integer value
+ * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
