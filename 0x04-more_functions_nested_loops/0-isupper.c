c1b3d3dfc47a5e9c0d911943f49895ba31246371
Salvation Makina <salvation.developer@gmail.com>
2022-09-15 15:35:51 +0000

    betty coding style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,11 +3,12 @@
 /**
  * _isupper - prototype, checks if given character is upper case
  * @num: integer character
- * Returns: 1 if true or 2 if false
+ * Description - Checks if given number is an upper case character
+ * Return: 1 if true or 2 if false
  */
 int _isupper(int num)
 {
-	if(num >= 65 && num <= 90)
+	if (num >= 65 && num <= 90)
 	{
 		return (1);
 	}
