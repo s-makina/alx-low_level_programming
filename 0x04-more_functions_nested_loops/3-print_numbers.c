891b74f9b69bcdc49552e499999ce754e71052b2
Salvation Makina <salvation.developer@gmail.com>
2022-09-15 16:20:31 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * Return: 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d");
+	}
+	print("\n");
+}
