d5b64f064c305770056810ddea0043df8ee639f6
Salvation Makina <salvation.developer@gmail.com>
2022-09-15 16:38:07 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 4 || a != 2)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	_putchar('\n');
+}
