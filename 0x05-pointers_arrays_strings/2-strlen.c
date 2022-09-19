6e1b7641a38efbaa62ce4372a6e54912ef2f4005
Salvation Makina <salvation.developer@gmail.com>
2022-09-19 19:17:06 +0000

    Completed task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: Char pointer
+ * Return: Int, String size
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
