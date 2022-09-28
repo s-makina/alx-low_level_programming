29c967a280ce0c2171d42bf6609468e3ae0fbb71
Salvation Makina <salvation.developer@gmail.com>
2022-09-28 18:43:06 +0000

    task 1

---
+++ 0x08-recursion/_print_rev_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: Char array to print
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int size = sizeof(s);
+
+	for (int i = 0; i < size; i++)
+	{
+		_putchar(s[i]);
+	}
+	printf("%d", "\n");
+}
