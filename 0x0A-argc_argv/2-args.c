90f2fc944eea39d3084120a41a9ef9274a68a6cf
Salvation Makina <salvation.developer@gmail.com>
2022-09-30 18:04:59 +0000

    task complete

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: Argument size
+ * @argv: Arguments parsed to the main function
+ * Return: 0 Success
+ */
+
+int main(int argc, int **argv)
+{
+	int size = argc - 1;
+
+	while (size--)
+	{
+		printf("%d\n", argv[size]);
+	}
+	return (0);
+}
