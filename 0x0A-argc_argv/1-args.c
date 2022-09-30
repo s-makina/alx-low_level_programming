a3f5a390e445d46683f346140b8493d7e90d3b78
Salvation Makina <salvation.developer@gmail.com>
2022-09-30 17:53:43 +0000

    Task 1 complete

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * description: prints size of arguments
+ * @argv: Arguments passed to the fuction
+ * @argc: size of arguments passed
+ * Return: 0 Success
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
