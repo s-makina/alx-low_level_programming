d0285b498b2a52f866cf56fe61140b3082de9e02
Salvation Makina <salvation.developer@gmail.com>
2022-09-30 17:40:25 +0000

    main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
-#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints first argument
+ * @argv: Comand line arguments
+ * @argc: Size of arguments parsed
+ * Return: 0, Success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
