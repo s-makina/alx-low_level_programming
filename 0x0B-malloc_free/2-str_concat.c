410637c4657123426bc224a96b579e19b0386d74
Salvation Makina <salvation.developer@gmail.com>
2022-10-04 18:42:10 +0000

    finish

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,21 +15,21 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, n, j;
 	int size;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 	{
 		*s1 = "";
 	}
-	if(s2 == NULL)
+	if (s2 == NULL)
 	{
 		*s2 = ""
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for(n = 0; s2[n] != '\0'; n++)
+	for (n = 0; s2[n] != '\0'; n++)
 		;
 	size = n + i + 1;
 	ptr = malloc(sizeof(char) * size);
-	while( j < i)
+	while (j < i)
 	{
 		ptr[j] = s1[j];
 		j++;
