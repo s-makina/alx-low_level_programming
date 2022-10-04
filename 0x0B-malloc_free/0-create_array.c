fbc353e1139201ca34b79949031dbee3a6eaad41
Salvation Makina <salvation.developer@gmail.com>
2022-10-04 15:41:24 +0000

    task 0 on malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ * @size: size of chatacter array
+ * @c: initialised letter
+ * Return: Pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	*arr = malloc(sizeof(*arr) * size);
+	arr[0] = c;
+	return (arr);
+}
