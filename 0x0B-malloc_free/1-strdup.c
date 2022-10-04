814fec9e5bc245a25c807e3bfba1ff65520f024b
Salvation Makina <salvation.developer@gmail.com>
2022-10-04 17:08:46 +0000

    space required around =

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	for (i=0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 	ptr = (char *)malloc(sizeof(char) * (i + 1));
 	if (ptr == NULL)
