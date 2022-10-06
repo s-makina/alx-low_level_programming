ac539d8051549c622e2494ab66ff5afec064cb9e
Salvation Makina <salvation.developer@gmail.com>
2022-10-06 19:53:30 +0000

    concate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i, k, g, s1size, size;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	for (s1size = 0; s1[s1size] != '\0'; s1size++)
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
-	for (g = 0; s2[g] != '\0'; g++)
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
-	if (g > n)
-		size = g;
-	else
-		size = n;
-	ptr = malloc(s1size + size + 1);
-	if (ptr == NULL)
-	{
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		ptr[i] = s1[i];
-	}
-	for (k = 0; k < n; k++, i++)
-	{
-		ptr[i] = s2[k];
-	}
-	ptr[i + 1] = '\0';
-	return (ptr);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
