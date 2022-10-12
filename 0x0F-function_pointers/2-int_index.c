dfabf1c4a9f4e6b156c8127170a233ef784a26f9
Salvation Makina <salvation.developer@gmail.com>
2022-10-12 19:24:22 +0000

    complete

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,14 +3,13 @@
 /**
  * int_index - searches for an integer
  * @array: Array of integers to be searched
- * @size: Size of the array 
+ * @size: Size of the array
  * @cmp: pointer function
  * Return: index of found element or -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
-{
-	int i;
+{	int i;
 
 	if (array && cmp)
 	{
