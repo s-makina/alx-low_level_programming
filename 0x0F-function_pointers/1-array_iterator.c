15bf402c83c67ba89a6fb1c5d53b16b69702b152
Salvation Makina <salvation.developer@gmail.com>
2022-10-12 19:14:01 +0000

    null pointer exception

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,5 +14,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);
+		if (array && action)
+			action(array[i]);
 }
