53ac8b1db987620e735a59c64332eb7ec1f6ea43
Salvation Makina <salvation.developer@gmail.com>
2022-10-13 17:31:38 +0000

    trial 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all parameters.
+ * @n: size of argments
+ * Return: Sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum, value, i;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		sum += value;
+
+	}
+
+	va_end(args);
+	return (sum);
+}
