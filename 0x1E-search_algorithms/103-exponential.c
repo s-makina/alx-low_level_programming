630c800bf71b24bcadf63991f3c1e69a268a358f
Salvation Makina <salvation.developer@gmail.com>
2023-04-14 13:14:17 +0000

    fix

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,13 +1,19 @@
 #include "search_algos.h"
 
 /**
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
   * @array: A pointer to the first element of the array to search.
-  * @left: The starting index of the sub array to search.
-  * @right: The ending index of the sub array to search.
-  * @value: Search value.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
   *
-  * Return: value or -1.
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
   */
+
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
@@ -35,12 +41,17 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 }
 
 /**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
   * @array: A pointer to the first element of the array to search.
-  * @size: Total number of elements in the array.
-  * @value: Search value.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
   *
-  * Return: value or -1.
-  **/
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 0, right;
