c020c212394a2a65864bd11706edc680b02868eb
Salvation Makina <salvation.developer@gmail.com>
2023-04-14 12:39:49 +0000

    traing white space error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
  * searches for a value in an array of integers using
  * the Linear search algorithm
  *
- * @array: pointer to the first element of the 
+ * @array: pointer to the first element of the
  * array to search in
  * @size: number of elements in array
  * @value: value to search for
